ede5faf74082aacb61160f38f6fb6f84f8ed60ca
Audrius Kucinskas <urza@nabaka.com>
2014-10-14 07:39:13 +0300

    woring autocomplete

--- searchController/ViewController.swift
+++ searchController/ViewController.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 import Foundation
+import Alamofire
 
 class ViewController: UIViewController {
 
+    let googleApiKey:String = ""
+    // Number of letters you need in searchbar to start sending requests to google
+    let kMinLettersForRequest = 4
+    
+    func returnGooglePredictionUrlString(input:String) -> String {
+        var predictionUrlString = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\(input)&types=geocode&language=en&key=\(self.googleApiKey)"
+        return predictionUrlString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+    }
+    
+    func retturnGoogleDetailUrlString(placeId:String) -> String {
+        var detailsUrlString = "https://maps.googleapis.com/maps/api/place/details/json?placeid=\(placeId)&key=\(self.googleApiKey)"
+        return detailsUrlString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+    }
+    
     var countryTable: UITableView!
-    var countryArray = ["Australia", "Singapore", "Malaysia", "United States", "Germany", "United Kingdom", "Kenya"]
-    var searchArray = [String]()
     var countrySearchController = UISearchController(searchResultsController: nil)
+    var currentPredictions: [[String]] = [[]]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var headerView = UIView(frame: CGRectMake(0.0, 0.0, self.view.bounds.width, 44.0))
+        var footerView = UIView(frame: CGRectMake(0.0, 0.0, self.view.bounds.width, 44.0))
+        var footerImageView = UIImageView(image: UIImage(named: "powered-by-google"))
+        footerImageView.center = footerView.center
+        footerView.addSubview(footerImageView)
         
         self.countryTable = UITableView(frame: CGRectMake(0.0, 20.0, self.view.bounds.width, self.view.bounds.height), style: UITableViewStyle.Plain)
-        self.countryTable.backgroundColor = UIColor.orangeColor()
         self.countryTable.registerClass(CountryTableViewCell.self, forCellReuseIdentifier: "myReusableCell")
         self.view.addSubview(self.countryTable)
         
@@ -31,8 +48,12 @@ class ViewController: UIViewController {
         self.countrySearchController.hidesNavigationBarDuringPresentation = false
         self.countrySearchController.dimsBackgroundDuringPresentation = false
         self.countrySearchController.searchBar.frame = CGRectMake(0.0, 0.0, CGRectGetWidth(self.countryTable.frame), 44.0)
+        self.countrySearchController.searchBar.delegate = self
+        self.countrySearchController.searchBar.showsCancelButton = false
+        self.countrySearchController.searchBar.placeholder = ""
         headerView.addSubview(self.countrySearchController.searchBar)
         self.countryTable.tableHeaderView = headerView
+        self.countryTable.tableFooterView = footerView
         
         self.countryTable.delegate = self
         self.countryTable.dataSource = self
@@ -57,12 +78,16 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.countryArray.count
+        if self.currentPredictions.count == 1 {
+            return 0
+        } else {
+            return self.currentPredictions.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: CountryTableViewCell = tableView.dequeueReusableCellWithIdentifier("myReusableCell", forIndexPath: indexPath) as CountryTableViewCell
-        cell.countryLabel.text = "\(self.countryArray[indexPath.row])"
+        cell.countryLabel.text = "\(self.currentPredictions[indexPath.row][0])"
         
         return cell
     }
@@ -71,32 +96,55 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UISearchResultsUpdating {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        let kMinLettersForRequest = 4
+        
         var numberOfCharacters = countElements(searchController.searchBar.text)
         
-        if numberOfCharacters >= kMinLettersForRequest {
-        
+        if numberOfCharacters >= self.kMinLettersForRequest {
+            googleApi(searchController.searchBar.text)
+        } else if numberOfCharacters < kMinLettersForRequest {
+            self.currentPredictions.removeAll(keepCapacity: false)
+            self.countryTable.reloadData()
         }
-        
-        println("Change in search results: \(searchController.searchBar.text)")
-        
-        println("Table Height: \(numberOfCharacters)")
-        
-//        self.countryTable.reloadData()
     }
     
-    func googleApi() {
-    
+    func googleApi(inputFromSearchBar:String) {
+        
+        var predictionUrlString = self.returnGooglePredictionUrlString(inputFromSearchBar)
+        
+        var jdata:AnyObject = []
+        var predictions:Array<AnyObject> = []
+        
+        let res = Alamofire.request(.GET, predictionUrlString)
+            .responseJSON { (_, _, JSON, _) in
+                jdata = JSON!
+                predictions = jdata["predictions"] as Array
+                self.currentPredictions.removeAll(keepCapacity: true)
+                for predIndex in predictions {
+                    var predictionPlaceIdArray = [predIndex["description"] as String, predIndex["place_id"] as String]
+                    self.currentPredictions.append(predictionPlaceIdArray)
+                    
+                self.countryTable.reloadData()
+                }
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
+extension ViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar){
+        self.countrySearchController.searchBar.showsCancelButton = false
+    }
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        self.countrySearchController.searchBar.showsCancelButton = false
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar){
+        println("Search Button Clicked")
+        self.countrySearchController.searchBar.showsCancelButton = false
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        self.countrySearchController.searchBar.showsCancelButton = false
+    }
+}
